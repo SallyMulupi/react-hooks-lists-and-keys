8a3ba3c27b059c663fdedd516cb0a8507d60ae02
SallyMulupi <mulupisally2gmail.com>
2022-08-04 12:03:09 +0300

    add comments

---
+++ src/components/ColorItem.js
@@ -0,0 +1,20 @@
+function ColorItem(props) {
+    return <li style={{ color: props.color }}>{props.color}</li>;
+  }
+  
+  // ColorList component
+  function ColorList() {
+    const colors = [
+      "firebrick",
+      "rebeccapurple",
+      "salmon",
+      "darkslategray",
+      "hotpink",
+    ];
+  
+    const colorElements = colors.map((color) => {
+      return <ColorItem key={color} color={color} />;
+    });
+    // etc
+  }
+  export default ColorItem
